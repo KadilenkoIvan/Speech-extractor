ab5a5abebad656a359ad94b7213c8f26ea16c3db
KadilenkoIvan <kadilenkoivan@gmail.com>
2024-07-16 17:56:35 +0700

    modified v1

--- extract_voice.py
+++ extract_voice.py
@@ -6,9 +6,15 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file_path", default='test.mp3',
                         help="Path of the audio file of which the voice is to be extracted.", type=str)
+    parser.add_argument("--out_path", default = 'speech.wav',
+                        help="Path of the output file", type=str)
+    parser.add_argument("--save_speech", default=True,
+                        help="Flag to save the audio with no speech, only other sounds", type=bool)
     args = parser.parse_args()
 
     file_path = args.file_path
+    out_path = args.out_path
+    save_speech = args.save_speech
 
     model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
                               model='silero_vad',
@@ -18,18 +24,31 @@ def main():
 
     sampling_rate = 16000
 
-    wav = read_audio('test.mp3', sampling_rate=sampling_rate)
+    wav = read_audio(file_path, sampling_rate=sampling_rate)
 
     # get speech timestamps from full audio file
     speech_timestamps = get_speech_timestamps(wav, model, sampling_rate=sampling_rate)
-    print(speech_timestamps)
+    if save_speech:
+        no_speech_timestamps = []
+        start = speech_timestamps[0]['start']
+        end = speech_timestamps[0]['end']
+        no_speech_timestamps.append({'start': 0, 'end': start})
+        for i, fragment in enumerate(speech_timestamps[1:(len(speech_timestamps))]):
+            start = end
+            no_speech_timestamps.append({'start': start, 'end': fragment['start']})
+            end = fragment['end']
+        start = speech_timestamps[-1]['end']
+        end = len(wav)
+        no_speech_timestamps.append({'start': start, 'end': end})
+        speech_timestamps = no_speech_timestamps
 
+    print(speech_timestamps)
     # # If timestamps are to be saved in a seperate .txt file
     # with open('timestamps.txt', 'w') as file:
     #     file.write(str(speech_timestamps))
 
     # Merging all speech chunks into one file
-    save_audio('speech.wav', collect_chunks(speech_timestamps, wav), sampling_rate=sampling_rate) 
+    save_audio(out_path, collect_chunks(speech_timestamps, wav), sampling_rate=sampling_rate) 
 
 if __name__ == "__main__":
     main()
